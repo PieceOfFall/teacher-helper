007267194a705c0ee842bdacff21582958775fe1
luyiwen <940313262@qq.com>
2025-10-23 11:07:08 +0800

    feat: 完成ai接入

--- src/main/kotlin/com/teacher/teacherhelper/config/AiConfig.kt
+++ src/main/kotlin/com/teacher/teacherhelper/config/AiConfig.kt
@@ -1,9 +1,22 @@
 package com.teacher.teacherhelper.config
 
+import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatOptions
+import jakarta.annotation.PostConstruct
+import org.springframework.ai.chat.client.ChatClient
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor
+import org.springframework.ai.chat.model.ChatModel
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class AiConfig {
+class AiConfig(
+    private val chatModel: ChatModel
+) {
 
+    @Bean
+    fun chatClient(): ChatClient = ChatClient.builder(chatModel)
+        .defaultAdvisors(SimpleLoggerAdvisor())
+        .defaultOptions(DashScopeChatOptions.builder().withTopP(0.7).build())
+        .build()
 
 }
